fd7dd156332a7aeca27c9d9e99dcdbebee72e57e
Simone Ghilotti <simoneghilotti@gmail.com>
2025-04-12 14:51:12 +0100

    navbar searchbar fixed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/x-icon" href="{{ asset('img/icon.png') }}">
+    <title>Dish Delight</title>
+    <link rel="stylesheet" href="{{ asset('bootstrapFiles/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/header.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
+</head>
 <body class="d-flex flex-column min-vh-100">
     @extends('layouts.layout')
     @section('navbarItem1', 'Login')
