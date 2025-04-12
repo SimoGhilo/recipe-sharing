c9e3a5bdcce76dd072c1c40498c964da990cc9d8
Simone Ghilotti <simoneghilotti@gmail.com>
2025-04-12 16:07:15 +0100

    working on auth

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RecipeController;
+use App\Http\Controllers\AuthController;
 
 Route::fallback(function () {
     return redirect()->route('error.index')->with('error', 'Page not found');
 });
+
+//TODO: Working on auth, create views, error below
+Route:get('/register', [AuthController::class, 'show'])->name('show.register');
+
+// Route:get('/login', [AuthController::class, 'show'])->name('show.login');
 
 Route::get('/', [RecipeController::class, 'getAllRecipes']);
 
@@ -14,3 +20,4 @@ Route::get('/recipe/{id}', [RecipeController::class, 'show'])->name('recipe.show
 Route::get('/error', [RecipeController::class, 'notFound'])->name('error.index');
 
 Route::get('/preview', [RecipeController::class, 'preview'])->name('preview');
+
